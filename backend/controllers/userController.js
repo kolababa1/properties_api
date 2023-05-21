c4c51f27cb7503033d06c478bdf0060213f3d2ac
kolababa1 <kolababa1@gmail.com>
2023-05-21 14:55:22 +0100

    setup basic folder structures

---
+++ backend/controllers/userController.js
@@ -0,0 +1,58 @@
+import User from "../models/userModel.js";
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+
+// @desc Register a new user
+// route POST /api/users
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, dateOfBirth, location, email, role } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    dateOfBirth,
+    location,
+    email,
+    role,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc Authenticates a user
+// route POST /api/users/auth
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/db.js";
+
+//Routes Folders
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 
-const PORT = 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
